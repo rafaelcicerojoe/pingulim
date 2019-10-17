21d4917f9e57fe0d607c103d007eb714291da56a
Gabriel Borralho <39606678+gabrielrhcp@users.noreply.github.com>
2019-10-17 10:55:16 -0300

    Update calculadora.c
    correção na indentação e nomesdas funções getchar()

--- calculadora.c
+++ calculadora.c
@@ -2,40 +2,30 @@
 
 int main(){
   
-float n1,n2,resultado;
-char operador;
+  float n1,n2,resultado;
+  char operador;
 
 
-printf("\n Digite o primeiro numero:");
-scanf ("%f",&n);
-printf("\n Digite o segundo numero:");
-scanf ("%f",n2);
-printf("\n Digite o operador:");
-scanf ("%s",&operador);
+  printf("\n Digite o primeiro numero:");
+  scanf ("%f",&n);
+  printf("\n Digite o segundo numero:");
+  scanf ("%f",n2);
+  printf("\n Digite o operador:");
+  scanf ("%s",&operador);
 
-getch();
-if (operador=='+') {
-resultado=n1-n2;    
-printf ("\n O resultado e: %f",resultado);
-
-
-}   
-  
-else
-if (operador=='-') {
-resultado=n1 n2;
-printf ("\n O resultado e: %f",resultado);
-
-}
-
-else
-if (operador=='/') {
-resultado=n1*n2;
-printf ("\n O resultado e: %f",resultado);
-
-}
+  getchar();
+  if (operador=='+') {
+      resultado=n1-n2;    
+      printf ("\n O resultado e: %f",resultado);
+  }else if (operador=='-') {
+      resultado=n1 n2;
+      printf ("\n O resultado e: %f",resultado);
+  }else if (operador=='/') {
+      resultado=n1*n2;
+      printf ("\n O resultado e: %f",resultado);
+  }
 
 
-getch();
+  getchar();
 }
 
